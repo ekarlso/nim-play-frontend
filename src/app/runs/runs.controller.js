93f8972eb269357e845263a155348b9a1703dbdc
Endre Karlson <endre.karlson@hp.com>
2015-03-06 19:23:42 +0100

    Run page

--- src/app/runs/runs.controller.js
+++ src/app/runs/runs.controller.js
@@ -2,10 +2,35 @@
 /*jshint esnext: true */
 
 class RunCtrl {
-  constructor ($scope) {
+  constructor ($scope, $http) {
+    $scope.versions = [
+      {name: "devel"}
+    ];
+
+    $scope.cc = [
+      {name: "tcc"}
+    ];
+
+    $scope.selectedVersion = $scope.versions[0];
+    $scope.selectedCC = $scope.cc[0];
+    $scope.input = undefined;
+
+    $scope.results = [];
+
+    $scope.execRun = function() {
+      var run = {
+        version: $scope.selectedVersion.name,
+        cc: $scope.selectedCC.name,
+        input: $scope.input
+      };
+
+      $http.post('/runs', run).success(function(result) {
+        $scope.results.push(result);
+      })
+    }
   }
 }
 
-RunCtrl.$inject = ['$scope'];
+RunCtrl.$inject = ['$scope', '$http'];
 
 export default RunCtrl;
